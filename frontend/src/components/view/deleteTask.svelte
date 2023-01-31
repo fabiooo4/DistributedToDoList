39818e78ad9b2c8da2aff2edaa201f25a607eca4
fabiooo4 <fabioirimie@gmail.com>
2023-01-31 19:44:05 +0100

    moved delete button to a new component

---
+++ frontend/src/components/view/deleteTask.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { deleteTask } from "../controller/fetchTasks";
+
+  export let task;
+</script>
+
+<button class="btn btn-error btn-xs btn-circle absolute left-0 self-center" on:click={()=>{deleteTask(task.id)}}>
+  <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
+</button>
--- frontend/src/components/view/task.svelte
+++ frontend/src/components/view/task.svelte
@@ -1,7 +1,7 @@
 <script>
-  import EditTask from "./editTask.svelte";
-  import { deleteTask } from "../controller/fetchTasks";
   import { toggleState } from "../controller/fetchTasks";
+  import DeleteTask from "./deleteTask.svelte";
+  import EditTask from "./editTask.svelte";
 
   export let task;
 </script>
@@ -28,9 +28,7 @@
       <EditTask {task} />
 
       <!--! Delete button -->
-      <button class="btn btn-error btn-xs btn-circle absolute left-0 self-center" on:click={()=>{deleteTask(task.id)}}>
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" /></svg>
-      </button>
+      <DeleteTask {task} />
     </div>
   </div>
 </div>
